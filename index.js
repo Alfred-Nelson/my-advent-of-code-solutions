b7416a03092ce4cb16fad51bcb88cdd8b3d69c10
Alfred Nelson <alfrednelson375@gmail.com>
2025-12-20 17:39:17 +0530

    fix: to accomodate any dsa sheet

--- index.js
+++ index.js
@@ -1,45 +1,96 @@
 import chalk from "yoctocolors";
-import { intro, outro } from "@clack/prompts";
+import { intro, outro, select } from "@clack/prompts";
 import ProjectProcess from "./utils/projectProcess";
 import helpers from "./utils/helpers";
+
+const IGNORE_LIST = ["node_modules", "utils", "input.txt", ".git"];
 
 const wrapperColor = (message) =>
   chalk.bgCyanBright(chalk.bold(" " + message + " "));
 
-intro(wrapperColor("Advent of Code TS/JS Solutions"));
+async function genericSelector() {
+  let iteration = 1;
+  let path = ".";
 
-let year, day, challenge;
+  while (!path.endsWith(".ts")) {
+    const filterCondition = (dir) => {
+      const isNotIgnored = !IGNORE_LIST.some(
+        (ignore) => dir.name.includes(ignore) || dir.name.includes("aoc")
+      );
+      if (iteration === 1) {
+        return dir.isDirectory() && isNotIgnored;
+      }
+      return isNotIgnored;
+    };
 
-year = await helpers.makeSelect({
-  question: "Choose Year :",
-  filterCondition: (item) => item.isDirectory() && item.name.includes("aoc"),
-});
+    const selected = await helpers.makeSelect({
+      path,
+      filterCondition,
+      labelFormatter: (dir) => dir.name,
+    });
 
-if (!!year?.name) {
-  day = await helpers.makeSelect({
-    question: "Choose Day :",
-    path: year.parentPath + "/" + year.name,
-    filterCondition: (item) => item.isDirectory() && item.name.includes("day"),
+    path = selected.parentPath + "/" + selected.name;
+    iteration++;
+  }
+
+  const command = "tsx " + path;
+
+  return { command, projectName: "challenge" };
+}
+
+async function aocSelector() {
+  let year, day, challenge;
+
+  year = await helpers.makeSelect({
+    question: "Choose Year :",
+    filterCondition: (item) => item.isDirectory() && item.name.includes("aoc"),
+  });
+
+  if (!!year?.name) {
+    day = await helpers.makeSelect({
+      question: "Choose Day :",
+      path: year.parentPath + "/" + year.name,
+      filterCondition: (item) =>
+        item.isDirectory() && item.name.includes("day"),
+    });
+  }
+
+  if (!!day?.name) {
+    challenge = await helpers.makeSelect({
+      question: "Choose Challenge :",
+      path: day.parentPath + "/" + day.name,
+      filterCondition: (item) =>
+        !item.isDirectory() && item.name.includes("challenge"),
+    });
+  }
+  const challengePath = challenge.parentPath + "/" + challenge.name;
+  const command = "tsx " + challengePath;
+  const challengeName = challenge.name.split(".")[0];
+
+  return { command, projectName: challengeName };
+}
+
+//   const challengePath = challenge.parentPath + "/" + challenge.name;
+//   const command = "tsx " + challengePath;
+//   const challengeName = challenge.name.split(".")[0];
+// }
+
+async function main() {
+  intro(wrapperColor("Code the day"));
+  const selection = await select({
+    message: "what solution should I show you ?",
+    options: [
+      { label: "aoc", value: "aoc" },
+      { label: "other", value: "other" },
+    ],
+  });
+  const processConfig = await (selection === "aoc"
+    ? aocSelector()
+    : genericSelector());
+  const challengeRunner = new ProjectProcess(processConfig);
+  challengeRunner.onClose(() => {
+    outro(wrapperColor("Make Progress Each Day"));
   });
 }
 
-if (!!day?.name) {
-  challenge = await helpers.makeSelect({
-    question: "Choose Challenge :",
-    path: day.parentPath + "/" + day.name,
-    filterCondition: (item) =>
-      !item.isDirectory() && item.name.includes("challenge"),
-  });
-}
-
-const challengePath = challenge.parentPath + "/" + challenge.name;
-const command = "tsx " + challengePath;
-const challengeName = challenge.name.split(".")[0];
-
-const challengeRunner = new ProjectProcess({
-  command,
-  projectName: challengeName,
-});
-challengeRunner.onClose(() => {
-  outro(wrapperColor("Make Progress Each Day"));
-});
+await main();
